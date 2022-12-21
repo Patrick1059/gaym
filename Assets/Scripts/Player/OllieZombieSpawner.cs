79fe916b7b595db8315d92a7fd591218e66335f4
Patrick1059 <patrick.mack1059@gmail.com>
2022-12-20 19:16:07 -0600

    added basic zombie controller, not done yet

--- Assets/Scripts/Player/OllieZombieSpawner.cs
+++ Assets/Scripts/Player/OllieZombieSpawner.cs
@@ -2,13 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileShooter : MonoBehaviour
+public class OllieZombieSpawner : MonoBehaviour
 {
     // The projectile prefab that will be instantiated when the player shoots
     public GameObject projectilePrefab;
-
-    // The force that will be applied to the projectile when it is shot
-    public float projectileForce = 10f;
 
     // The rate at which the player can shoot projectiles
     public float fireRate = 0.5f;
@@ -26,29 +23,12 @@ public class ProjectileShooter : MonoBehaviour
         // If the player is holding down the left mouse button
         if (Input.GetMouseButton(0))
         {
-            // Increment the fire timer by the time that has passed since the last frame
-            
-
             // If the fire timer has reached the fire rate, shoot a projectile and reset the fire timer
             if (fireTimer >= fireRate)
             {
-                // Get the position of the mouse cursor in world space
-                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
                 // Create a new projectile at the position of the player
                 GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
-
-                // Get the Rigidbody2D component of the projectile
-                Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
-
-                // Calculate the direction from the player to the mouse cursor
-                Vector2 direction = mousePos - transform.position;
-
-                // Normalize the direction vector to remove any scaling caused by the distance between the player and the mouse cursor
-                direction.Normalize();
-
-                // Apply the force to the Rigidbody2D component of the projectile in the calculated direction
-                rb.AddForce(direction * projectileForce, ForceMode2D.Impulse);
 
                 // Destroy the projectile after a certain amount of time
                 Destroy(projectile, projectileLifetime);
---
+++ Assets/Scripts/Player/ZombieController.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    public float speed; // The speed at which the game object will move
+    public float attackRange; // The distance at which the game object will attack the enemy
+    public int damage; // The amount of damage that the game object will deal upon contact with the enemy
+    public float attackInterval = 0.5f;
+    private float timeSinceLastAttack;
+
+    private GameObject closestEnemy; // A reference to the closest enemy game object
+
+    void Update()
+    {
+        // Find the closest enemy game object
+        closestEnemy = FindClosestEnemy();
+
+        if (closestEnemy != null)
+        {
+            // Calculate the distance to the enemy
+            float distanceToEnemy = Vector3.Distance(transform.position, closestEnemy.transform.position);
+
+            // If the distance to the enemy is within the attack range, attack the enemy
+            if (distanceToEnemy <= attackRange)
+            {
+                AttackEnemy();
+            }
+            // If the distance to the enemy is outside the attack range, move towards the enemy
+            else
+            {
+                MoveTowardsEnemy();
+            }
+        }
+    }
+
+    // This function finds the closest enemy game object
+    private GameObject FindClosestEnemy()
+    {
+        // Find all enemy game objects in the scene
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        // Initialize the closest enemy to be null
+        GameObject closest = null;
+
+        // Initialize the distance to the closest enemy to be infinity
+        float closestDistance = Mathf.Infinity;
+
+        // Iterate through each enemy game object
+        foreach (GameObject enemy in enemies)
+        {
+            // Calculate the distance to the enemy
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            // If the distance to the enemy is smaller than the current closest distance, set the closest enemy to be this enemy
+            if (distanceToEnemy < closestDistance)
+            {
+                closest = enemy;
+                closestDistance = distanceToEnemy;
+            }
+        }
+
+        // Return the closest enemy game object
+        return closest;
+    }
+
+    // This function moves the game object towards the closest enemy
+    private void MoveTowardsEnemy()
+    {
+        // Calculate the direction to the enemy
+        Vector3 direction = closestEnemy.transform.position - transform.position;
+        direction.Normalize();
+
+        // Move the game object towards the enemy
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    // This function attacks the closest enemy
+    private void AttackEnemy()
+{
+    // Increment the time since the last attack
+    timeSinceLastAttack += Time.deltaTime;
+
+    // If the attack interval has been reached, deal damage to the enemy and reset the time since the last attack
+    if (timeSinceLastAttack >= attackInterval)
+    {
+        // Deal damage to the enemy
+        closestEnemy.GetComponent<EnemyHealth>().TakeDamage(damage);
+
+        // Reset the time since the last attack
+        timeSinceLastAttack = 0;
+    }
+}
+}
